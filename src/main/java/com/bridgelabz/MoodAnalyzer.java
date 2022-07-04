9987f4748dce305d967471f59155122e11eda376
Annu <annusinghraj1998@gmail.com>
2022-07-04 23:18:46 +0530

    ADDED: Ability to find mood according to message

--- src/main/java/com/bridgelabz/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/MoodAnalyzer.java
@@ -1,8 +1,8 @@
 package com.bridgelabz;
 
 public class MoodAnalyzer {
-    public String analyzeMood(String message){
-        if(message.contains("I am in sad mood"))
+    public String analyzeMood(String message) {
+        if (message.contains("I am in sad mood"))
             return "SAD";
         else
             return "HAPPY";
